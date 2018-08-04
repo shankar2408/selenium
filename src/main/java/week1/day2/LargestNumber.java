e592b75909b9fa5edd5fe6df96a1f0ea4447ba92
Shankar <shankarec24@gmail.com>
2018-08-04 22:19:32 +0530

    find largest number

---
+++ src/main/java/week1/day2/LargestNumber.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+int c=30;
+if ((a>b) & (a>c))
+{
+System.out.println("largest number is a:" +a);	
+} else
+if((b>c) & (b>a))
+{
+	System.out.println("largest number is b:" +b);
+	}
+else
+{
+	System.out.println("largest number is c:" +c);
+	}
+
+	}
+
+}
